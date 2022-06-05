53c7a1bc2c0983b0cb23c8d27a1b6139ee6ba74c
crypto_jedi <ybn1992@gmail;.com>
2022-06-05 22:45:35 +0800

    hw4

---
+++ hw4/src/hw4_assets/src/index.js
@@ -0,0 +1,19 @@
+import { hw4 } from "../../declarations/hw4";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await hw4.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
